10194c17b61308e0ea060b85d956fa5eaa540566
Cade Conner <cadeconner8@gmail.com>
2024-08-05 22:47:10 -0500

    experimenting with project displays

--- components/ProjectDisplay.tsx
+++ components/ProjectDisplay.tsx
@@ -1,14 +1,23 @@
 import ProjectModel from "@/models/ProjectModel"
 
 
-export const ProjectDisplay: React.FC<{project: ProjectModel}> = (props) => {
+export const ProjectDisplay: React.FC<{ project: ProjectModel }> = (props) => {
 
     const portProject: ProjectModel = props.project;
+
+
+
     return (
-        <div className="text-center">
+        <div className="flex flex-col justify-center items-center text-center">
             <div>{portProject.name}</div>
-            <img src="https://side-projs-blob.s3.amazonaws.com/portfolio/demeterLight.png" width="640" height="480"></img>
-            <img src={portProject.imgUrl} className="w-1/4 h-1/4"></img>
+
+            <div className="video-container p-5">
+                {portProject.imgUrl.endsWith("mp4") ?
+                    <video autoPlay loop muted className="object-cover w-full rounded-md" src={portProject.imgUrl}></video>
+                    :
+                    <img src={portProject.imgUrl} className="object-cover w-[400%] rounded-md" alt={`Screenshot of ${portProject.name} project`}></img>
+                }
+            </div>
             <div>{portProject.desc}</div>
             <div>{portProject.type}</div>
         </div>
