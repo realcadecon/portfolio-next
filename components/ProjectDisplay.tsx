6f6d94de91dff5d9538042e3d477ab3cb3bcc759
Cade Conner <cadeconner8@gmail.com>
2024-08-09 15:35:43 -0500

    take out redundant div to try and fix mobile formatting issue

--- components/ProjectDisplay.tsx
+++ components/ProjectDisplay.tsx
@@ -22,14 +22,12 @@ export const ProjectDisplay: React.FC<{ project: ProjectModel }> = (props) => {
     }, {scope: container})
 
     return (
-        <div ref={container}>
-            <a className="relative container" href="/" onMouseEnter={() => {if(timeline) timeline.play()}} onMouseLeave={() => {if(timeline) timeline.reverse()}} onClick={() => gsap.to(".container", { display: "hidden" })}>
-                <div className={`absolute text-white font-bold md:font-normal bottom-[3%] left-[5%] z-20 init-overlay-${portProject.id}`}>{portProject.name}</div>
-                <video autoPlay playsInline loop muted className="rounded-3xl w-full object-cover" src={portProject.imgUrl}></video>
-                <div className={`absolute shadow-overlay left-0 right-0 bg-gradient-to-t from-black bottom-0 h-[50%] z-10 rounded-b-3xl`}></div>
-                <div className={`absolute bottom-[4%] right-[4%] inline-block py-[1%] px-[3%] rounded-full bg-primary z-20 text-xs font-semibold font-sans text-gray-200 init-overlay-${portProject.id}`}>{portProject.type}</div>
-                <div className={`absolute z-20 bottom-[6%] left-[5%] w-[80%] text-white opacity-0`} id={`desc-${portProject.id}`}>{portProject.desc}</div>
-            </a>
-        </div>
+        <a ref={container} className="relative container" href="/" onMouseEnter={() => {if(timeline) timeline.play()}} onMouseLeave={() => {if(timeline) timeline.reverse()}} onClick={() => gsap.to(".container", { display: "hidden" })}>
+            <div className={`absolute text-white font-bold md:font-normal bottom-[3%] left-[5%] z-20 init-overlay-${portProject.id}`}>{portProject.name}</div>
+            <video autoPlay playsInline loop muted className="rounded-3xl w-full object-cover" src={portProject.imgUrl}></video>
+            <div className={`absolute shadow-overlay left-0 right-0 bg-gradient-to-t from-black bottom-0 h-[50%] z-10 rounded-b-3xl`}></div>
+            <div className={`absolute bottom-[4%] right-[4%] inline-block py-[1%] px-[3%] rounded-full bg-primary z-20 text-xs font-semibold font-sans text-gray-200 init-overlay-${portProject.id}`}>{portProject.type}</div>
+            <div className={`absolute z-20 bottom-[6%] left-[5%] w-[80%] text-white opacity-0`} id={`desc-${portProject.id}`}>{portProject.desc}</div>
+        </a>
     )
 }
